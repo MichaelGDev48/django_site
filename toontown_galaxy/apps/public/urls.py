7c8598bd9a82afee34fe9d4f38a93b92915e87b1
Galaxy711 <66341738+Galaxy711@users.noreply.github.com>
2022-02-06 09:59:15 -0500

    Add apply.html

--- toontown_galaxy/apps/public/urls.py
+++ toontown_galaxy/apps/public/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("", views.index, name='landing'),
     path("about", views.about, name='about'),
     path('partner_apply', views.partner_apply, name='partner_apply'),
-    path('staff', views.staff, name='staff')
+    path('staff', views.staff, name='staff'),
+    path('apply', views.apply, name='apply')
    # path("accounts/profile", views.ProfileView.as_view(), name="profile"),
     
  ]
